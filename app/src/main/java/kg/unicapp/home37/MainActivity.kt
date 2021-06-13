f3b4f73d074465c7d86f2388dbffed2edaf8abd9
agamonch <jnkyrgyz@gmail.com>
2021-06-13 19:17:34 +0300

    added toolbar menu with 4 items and with toast messages

--- app/src/main/java/kg/unicapp/home37/MainActivity.kt
+++ app/src/main/java/kg/unicapp/home37/MainActivity.kt
@@ -3,7 +3,8 @@ package kg.unicapp.home37
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -23,7 +24,6 @@ class MainActivity : AppCompatActivity() {
 
         button.setOnClickListener {
             if(edittext.length()>0) {
-
                 val intent1 = Intent(this, MainActivity2::class.java)
                 intent1.putExtra("Text", edittext.text.toString())
                 startActivity(intent1)
@@ -32,7 +32,20 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Edittext is EMPTY", Toast.LENGTH_LONG).show()
                 }
         }
-
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_settings ->  Toast.makeText(this, "Нечего настраивать", Toast.LENGTH_SHORT).show()
+            R.id.about_app -> Toast.makeText(this, "Это мое приложение", Toast.LENGTH_SHORT).show()
+            R.id.add_something -> Toast.makeText(this, "Позже добавите", Toast.LENGTH_SHORT).show()
+            R.id.close_app -> finish()
+        }
+        return true
+    }
 }
