839ddf8383105dcd5177f616b9e3151f55847b5e
agamonch <jnkyrgyz@gmail.com>
2021-06-13 16:50:23 +0300

    added Toast for button onClick in mainactivity

--- app/src/main/java/kg/unicapp/home37/MainActivity.kt
+++ app/src/main/java/kg/unicapp/home37/MainActivity.kt
@@ -3,8 +3,10 @@ package kg.unicapp.home37
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,15 +22,17 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
+            if(edittext.length()>0) {
 
-            val intent1 = Intent(this, MainActivity2::class.java)
-            intent1.putExtra("Text", edittext.text.toString())
-            startActivity(intent1)
-
+                val intent1 = Intent(this, MainActivity2::class.java)
+                intent1.putExtra("Text", edittext.text.toString())
+                startActivity(intent1)
+            }
+            else{
+                Toast.makeText(this, "Edittext is EMPTY", Toast.LENGTH_LONG).show()
+                }
         }
 
     }
-
-
 
 }
